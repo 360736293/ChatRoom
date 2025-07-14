3b9473906a7b6ea0d739bdc0b1e13f2b3a5e3fcf
lostmilkyway <49705090+360736293@users.noreply.github.com>
2025-07-14 16:49:41 +0800

    Update ChatConstants.java

--- src/main/java/org/example/constant/ChatConstants.java
+++ src/main/java/org/example/constant/ChatConstants.java
@@ -5,14 +5,15 @@ package org.example.constant;
  */
 public class ChatConstants {
     public static final String DEFAULT_CHANNEL = "频道1";
+
     public static final String SYSTEM_USER_ID = "system";
+
     public static final String SYSTEM_USERNAME = "系统";
+
     public static final String SYSTEM_AVATAR = "S";
 
     // 消息相关常量
     public static final int MAX_MESSAGE_SIZE = 1024 * 1024 * 1024; // 1GB
-    public static final String DEFAULT_CONTENT_TYPE = "text";
-    public static final String IMAGE_CONTENT_TYPE = "image";
 
     private ChatConstants() {
         // 防止实例化
