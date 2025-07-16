86b764b8632e07150bbcf9614d7329ed0d204508
360736293 <360736293@qq.com>
2025-07-16 23:05:20 +0800

    补充导包

--- src/main/java/org/example/controller/ChatController.java
+++ src/main/java/org/example/controller/ChatController.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.constant.ChatConstants;
 import org.example.service.ChannelService;
 import org.example.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -2,6 +2,7 @@ package org.example.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.example.constant.ChatConstants;
 
 @Setter
 @Getter
